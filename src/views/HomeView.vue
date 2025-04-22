bcd0cece04ad1f587116de56a5fe9692c42f3410
Hannah-Hoover <hd3611@wayne.edu>
2025-04-22 14:17:13 -0400

    handle showing the thank you message for 5 seconds

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <ContactForm v-if="!showThankYou" @submit-success="handleSubmit" />
+    <ThankYou v-else />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { defineComponent, ref } from "vue";
+import ContactForm from "@/components/ContactForm.vue";
+import ThankYou from "@/components/ThankYou.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    HelloWorld,
+    ContactForm,
+    ThankYou,
+  },
+  setup() {
+    const showThankYou = ref(false);
+
+    const handleSubmit = () => {
+      showThankYou.value = true;
+      setTimeout(() => {
+        showThankYou.value = false;
+      }, 5000);
+    };
+
+    return {
+      showThankYou,
+      handleSubmit,
+    };
   },
 });
 </script>
+
+<style scoped lang="scss"></style>

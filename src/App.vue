74e57f9dce664f530a29280d47f77c9b8d4ecb4c
Hannah-Hoover <hd3611@wayne.edu>
2025-04-22 14:14:34 -0400

    insert and format logo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <img
+      alt="GreenTechlogo"
+      id="greentechlogo"
+      src="@/assets/greentechlogo.png"
+    />
   </nav>
   <router-view />
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=ABeeZee&display=swap");
+
 html,
 body,
 #app {
   min-height: 100dvh;
-  background-color: #fff;
+  background-color: #f4f2ee;
+}
+
+#greentechlogo {
+  width: 122.83px;
+  height: 40px;
+  position: absolute;
+  top: 30px;
+  left: 35px;
 }
 
 .fade-enter-active,
